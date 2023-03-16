2ee5d8b74d45b3d264e3ba814ede291397a1aa06
Serdar GÜL <serdargul@outlook.com>
2023-03-16 14:36:24 +0300

    Açıklama satırları eklendi

--- SourceCode/CSharpHelperMethods.UI/OnemliBilgiler.cs
+++ SourceCode/CSharpHelperMethods.UI/OnemliBilgiler.cs
@@ -2,8 +2,18 @@ using System.Collections.Generic;
 
 namespace CSharpHelperMethods.UI
 {
+    /// <summary>
+    /// Burada kod içerisinde gerekli olabilecek bazı dictionary, list gibi sabitlerin tutulduğu
+    /// Bir sınıftır
+    /// </summary>
     public class OnemliBilgiler
     {
+        /// <summary>
+        /// Panel adları ve kodları
+        /// Burada beraber tutularak
+        /// Combobox'ta yapılan değişikliğe göre
+        /// İlgili panelin açılabilmesi için gerekli bilgilerin tutulduğu Dictionary'dir.
+        /// </summary>
         public static Dictionary<int, string> PanelListesi = new Dictionary<int, string>
         {
             {0, "pnlMetinIslemleri"},
